8ef577b8d723fabeebd214689122b3aaead896eb
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 13:23:56 -0300

    Define DEFAULT_AUTHENTICATION_CLASSES simpleJwt

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'veiculos',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -126,5 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
 }
