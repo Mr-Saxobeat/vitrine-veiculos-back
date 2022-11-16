2804dd1d1ceae510ed8ea89a4ec623f8add93c0b
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 00:51:53 -0300

    Cria views para veiculos

--- veiculos/views.py
+++ veiculos/views.py
@@ -1,21 +1,48 @@
-from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
-from veiculos.serializers import GroupSerializer, UserSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from veiculos.models import Veiculo
+from veiculos.serializers import VeiculoSerializer
 
+@csrf_exempt
+def veiculo_list(request):
+    """
+    Lista todos veículos ou cria um novo.
+    """
+    if request.method == 'GET':
+        veiculos = Veiculo.objects.all()
+        serializer = VeiculoSerializer(veiculos, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = VeiculoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, stats=400)
 
-class UserViewSet(viewsets.ModelViewSet):
+def veiculo_detail(request, pk):
     """
-    API endpoint that allows users to be viewed or edited.
+    Recupera, atualiza ou deleta um veículo.
     """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    try:
+        veiculo = Veiculo.objects.get(pk=pk)
+    except Veiculo.DoesNotExist:
+        return HttpResponse(status=404)
 
+    if request.method == 'GET':
+        serializer = VeiculoSerializer(veiculo)
+        return JsonResponse(serializer.data)
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = VeiculoSerializer(veiculo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        veiculo.delete()
+        return HttpResponse(status=204)
