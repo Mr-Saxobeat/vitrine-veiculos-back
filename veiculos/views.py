f0de5f9f61f1c664a387e986ef91d2e22b9dc724
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 13:24:11 -0300

    Remove lib nao usada

--- veiculos/views.py
+++ veiculos/views.py
@@ -1,6 +1,5 @@
 from veiculos.models import Veiculo
 from veiculos.serializers import VeiculoSerializer
-from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework import permissions
 
