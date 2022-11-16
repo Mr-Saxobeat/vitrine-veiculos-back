a969e687431918c033ba65202fea0c9b5a179152
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 13:24:46 -0300

    Remove urls nao usadas e adiciona de autenticacao

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 from django.contrib import admin
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 router = routers.DefaultRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('vitrine/', include('veiculos.urls'))
+    path('api/', include('veiculos.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
