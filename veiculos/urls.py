feb5db73627e174a2f47dfb362fadf63ddc0704f
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 10:37:37 -0300

    Adiciona suffix_patterns

--- veiculos/urls.py
+++ veiculos/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from veiculos import views
 
 urlpatterns = [
     path('veiculos/', views.veiculo_list),
     path('veiculos/<int:pk>/', views.veiculo_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
