458f3ab8ee52dd86415079e741429db51684a659
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 00:18:19 -0300

    Corrige organizacao das pastas

---
+++ veiculos/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from veiculos.models import Veiculo
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class VeiculoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ['id', 'Nome', 'Marca', 'Modelo', 'Valor']
