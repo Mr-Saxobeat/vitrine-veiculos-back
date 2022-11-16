7d62f2fcb623f2ac104ed9617e927bdd77b7833d
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 11:29:45 -0300

    Remove Nome de Veiculo e corrige Valor

--- veiculos/models.py
+++ veiculos/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class Veiculo(models.Model):
-    Nome = models.CharField(max_length=50)
     Marca = models.CharField(max_length=50)
     Modelo = models.CharField(max_length=50)
-    Valor = models.DecimalField(max_digits=6, decimal_places=2)
+    Valor = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         ordering = ['Valor']
