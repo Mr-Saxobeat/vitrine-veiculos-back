0ef17e15568169351ea3525592837b671c5d859d
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 12:08:24 -0300

    Adiciona url admin

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from veiculos import views
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('vitrine/', include('veiculos.urls'))
 ]
