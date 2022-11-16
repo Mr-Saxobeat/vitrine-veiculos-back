c99d2dfd6a34e37257e7fe5bb4bffc16d5d23fe4
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 11:20:47 -0300

    Utiliza genericViews

--- veiculos/urls.py
+++ veiculos/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from veiculos import views
 
 urlpatterns = [
-    path('veiculos/', views.veiculo_list),
-    path('veiculos/<int:pk>/', views.veiculo_detail)
+    path('veiculos/', views.VeiculoList.as_view()),
+    path('veiculos/<int:pk>/', views.VeiculoDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- veiculos/views.py
+++ veiculos/views.py
@@ -1,48 +1,18 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from veiculos.models import Veiculo
 from veiculos.serializers import VeiculoSerializer
+from rest_framework import generics
 
-@csrf_exempt
-def veiculo_list(request):
+
+class VeiculoList(generics.ListCreateAPIView):
     """
     Lista todos veículos ou cria um novo.
     """
-    if request.method == 'GET':
-        veiculos = Veiculo.objects.all()
-        serializer = VeiculoSerializer(veiculos, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = VeiculoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, stats=400)
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
 
-def veiculo_detail(request, pk):
-    """
+class VeiculoDetail(generics.RetrieveUpdateDestroyAPIView):
+    """"
     Recupera, atualiza ou deleta um veículo.
     """
-    try:
-        veiculo = Veiculo.objects.get(pk=pk)
-    except Veiculo.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = VeiculoSerializer(veiculo)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = VeiculoSerializer(veiculo, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        veiculo.delete()
-        return HttpResponse(status=204)
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
