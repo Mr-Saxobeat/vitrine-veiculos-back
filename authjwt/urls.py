3fb28eebdba98ee4761f266df553996357acd72e
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-18 16:29:27 -0300

    Corrige urls

--- authjwt/urls.py
+++ authjwt/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import RegisterView
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth-register'),
-    path('login/', TokenObtainPairView.as_view(), name='auth-login'),
+    path('login/', LoginView.as_view(), name='auth-login'),
+    path('logout/', LogoutView.as_view(), name='auth-logout'),
     path('refresh/', TokenRefreshView.as_view(), name='auth-refresh'),
 ]
