7276761d9bd46a171fcbaaff37083416e7aae90e
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-18 16:29:40 -0300

    Cria login e logout views

--- authjwt/views.py
+++ authjwt/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 from rest_framework.response import Response
-
-from authjwt.serializers import RegisterSerializer, UserSerializer
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from authjwt.serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
 
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
@@ -13,3 +17,18 @@ class RegisterView(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "message": "User Created Successfully.  Now perform Login to get your token",
         })
+
+class LoginView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = RefreshToken(request.data['refresh'])
+            refresh_token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
